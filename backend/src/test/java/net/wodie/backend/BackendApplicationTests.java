311bf1a6f575b88d5b3a654b3fc90201cd5f841a
wodie99 <wodie99@wodie.net>
2022-06-25 11:19:17 +0200

    add backend - spring boot with: lombok, Spring Web, Spring Reactive Web, Spring Data MongoDB

---
+++ backend/src/test/java/net/wodie/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wodie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
