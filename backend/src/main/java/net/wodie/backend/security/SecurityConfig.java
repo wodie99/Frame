c0ca279b00bc5109e8fe78486d1c01df94abd844
wodie99 <wodie99@wodie.net>
2022-06-27 14:14:19 +0200

    add spring-boot-web-security and minimal SecurityConfig. Test only against Postman

---
+++ backend/src/main/java/net/wodie/backend/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package net.wodie.backend.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("java:S1874")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // more config..
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
